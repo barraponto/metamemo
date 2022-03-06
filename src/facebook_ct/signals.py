bee19b676e32aa0131fd337e496769dcfd8b160c
Capi Etheriel <barraponto@gmail.com>
2022-03-06 02:52:21 -0300

    feat(facebook_ct): connect signal to celery task

--- src/facebook_ct/signals.py
+++ src/facebook_ct/signals.py
@@ -1,9 +1,19 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from purl import URL
+
+from .tasks import get_facebook_data
 
 
 @receiver(post_save, sender="core.MemoSource")
 def on_source_save(sender, instance, created, **kwargs):
-    if not created:
+    # only run on new facebook.com profile sources
+    if not created or not URL(instance.url).host().endswith("facebook.com"):
         return
-    # add task here
+
+    try:
+        username = URL(instance.url).path().split("/").pop(1)
+    except IndexError:
+        pass
+    else:
+        get_facebook_data.delay(username)
